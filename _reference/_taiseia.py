b4401cdfd0b892239676ad73a17d092eb131f35a
Phantas Weng <bensonuong@gmail.com>
2021-01-15 13:49:45 +0800

    Add taiSEIA case

--- _reference/_taiseia.py
+++ _reference/_taiseia.py
@@ -1,61 +1,29 @@
 """ TaiSEIA 101 """
 from enum import Enum
 
-class Power(Enum):
-    Off = 0
-    On = 1
+ID: H'00
+HEX: 0x00
+Type: ENUM
+name: 電源
+關閉: 0
+開啟: 1
 
-class OperationMode(Enum):
-    Auto = 0
-    Dry = 1
-    Cool = 2
-    Heat = 3
-    Fan = 4
+ID: H'01
+HEX: 0x01
+Type: ENUM
+name: 模式
+冷氣: 0
+除濕: 1
+送風: 2
+自動: 3
+暖氣: 4
 
-class AirSwingUD(Enum):
-    Auto = -1
-    Up = 0
-    UpMid = 3
-    Mid = 2
-    DownMid = 4
-    Down = 1
+ID: H'02
+HEX: 0x02
+name: 風速
+Type: ENUM
+# 0 ~ 15 級
+Auto: 0
+Min: 1
+Max: 5
 
-class AirSwingLR(Enum):
-    Auto = -1
-    Left = 1
-    LeftMid = 5
-    Mid = 2
-    RightMid = 4
-    Right = 0
-
-class EcoMode(Enum):
-    Auto = 0
-    Powerful = 1
-    Quiet = 2
-
-class AirSwingAutoMode(Enum):
-    Disabled = 1
-    Both = 0
-    AirSwingLR = 3
-    AirSwingUD = 2
-
-class FanSpeed(Enum):
-    Auto = 0
-    Low = 1
-    LowMid = 2
-    Mid = 3
-    HighMid = 4
-    High = 5
-
-class dataMode(Enum):
-    Day = 0
-    Week = 1
-    Month = 2
-    Year = 4
-
-class NanoeMode(Enum):
-    Unavailable = 0
-    Off = 1
-    On = 2
-    ModeG = 3
-    All = 4
